2bc2c19b395036f37be6877ba12087510367246f
Hugo201120 <hugo@hugo>
2025-05-19 13:48:47 +0200

    Carpetas y html

---
+++ sql/ScriptTablas.sql
@@ -0,0 +1,50 @@
+
+-- Base de datos: portal_eventos_sostenibles
+
+CREATE TABLE Usuario (
+    id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL,
+    correo VARCHAR(100) NOT NULL UNIQUE,
+    contraseña VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Organizador (
+    id_organizador INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL,
+    contacto VARCHAR(150) NOT NULL
+);
+
+CREATE TABLE Categoria (
+    id_categoria INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Ubicacion (
+    id_ubicacion INT PRIMARY KEY AUTO_INCREMENT,
+    tipo ENUM('online', 'presencial') NOT NULL,
+    direccion VARCHAR(200),
+    url VARCHAR(200)
+);
+
+CREATE TABLE Evento (
+    id_evento INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(150) NOT NULL,
+    fecha DATE NOT NULL,
+    duracion INT NOT NULL,
+    descripcion TEXT,
+    id_categoria INT,
+    id_ubicacion INT,
+    id_organizador INT,
+    FOREIGN KEY (id_categoria) REFERENCES Categoria(id_categoria),
+    FOREIGN KEY (id_ubicacion) REFERENCES Ubicacion(id_ubicacion),
+    FOREIGN KEY (id_organizador) REFERENCES Organizador(id_organizador)
+);
+
+CREATE TABLE Inscripcion (
+    id_usuario INT,
+    id_evento INT,
+    fecha_inscripcion DATE NOT NULL,
+    PRIMARY KEY (id_usuario, id_evento),
+    FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario),
+    FOREIGN KEY (id_evento) REFERENCES Evento(id_evento)
+);
